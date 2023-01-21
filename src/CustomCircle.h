26166a47a92bd0788349143677b85851e56f6be0
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2023-01-22 02:29:17 +0900

    Create CustomCircle.h

---
+++ src/CustomCircle.h
@@ -0,0 +1,22 @@
+//
+//  CustomCircle.h
+//  ofBox2d_test
+//
+//  Created by Ricky O'kawa on 2023/01/22.
+//
+
+//#ifndef CustomCircle_h
+//#define CustomCircle_h
+
+//#include <stdio.h>
+#pragma once
+#include "ofMain.h"
+#include "ofxBox2d.h"
+
+class CustomCircle : public ofxBox2dCircle {
+    public :
+    void draw(); //draw a circle
+};
+
+
+//#endif /* CustomCircle_h */
